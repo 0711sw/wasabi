cdf715acffc9f7c53ef5237e336788e9ee3885c0
Andreas Haufler <andreas.haufler@durablox.com>
2025-05-14 14:49:43 +0200

    Next step of sending data to X-Ray

--- src/web/info.rs
+++ src/web/info.rs
@@ -1,4 +1,8 @@
+use opentelemetry::KeyValue;
+use opentelemetry::trace::TraceContextExt;
 use serde_json::json;
+use tracing::{Span, event};
+use tracing_opentelemetry::OpenTelemetrySpanExt;
 use warp::Filter;
 use warp::filters::BoxedFilter;
 
@@ -12,18 +16,17 @@ pub fn get_info_route() -> BoxedFilter<(impl warp::Reply,)> {
 #[tracing::instrument(level = "debug",
     name = "GET /info/v1",
     skip_all,
-    fields(http.request.method = "GET",
-           http.url = "1",
-           http.method = "2",
-           http.status_code = 404,
-           http.content_length = 5,
-              http.route = "/info/v112",
-              http.response.status_code = 405,
-           http.response.content_length = 8,
-           span.kind = "server",
-              url.path = "/info/v1")
+    fields(http.method = "GET",
+           http.url = "/info/v1")
 )]
 async fn handle_get_info() -> Result<impl warp::Reply, warp::Rejection> {
+    Span::current()
+        .context()
+        .span()
+        .set_attribute(KeyValue::new("http.status_code", 403));
+
+    event!(tracing::Level::DEBUG, "http.status_code" = 404);
+
     Ok(warp::reply::json(&json!({
             "app": crate::APP_NAME.clone(),
             "version": crate::APP_VERSION.clone(),
