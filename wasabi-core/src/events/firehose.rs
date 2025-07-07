894511df53859ec627da57887cc8089abf1622b2
Andreas Haufler <andreas.haufler@durablox.com>
2025-07-07 13:56:23 +0200

    Enablkes batch uploads for events towards Firehose

--- wasabi-core/src/events/firehose.rs
+++ wasabi-core/src/events/firehose.rs
@@ -1,4 +1,5 @@
 use crate::events::{Event, EventRecorder};
+use crate::tools::system;
 use crate::{APP_NAME, CLUSTER_ID};
 use async_trait::async_trait;
 use aws_sdk_firehose::Client;
@@ -8,6 +9,16 @@ use regex::Regex;
 use serde::Serialize;
 use std::env;
 use std::sync::LazyLock;
+use std::time::Duration;
+use tokio::sync::mpsc;
+
+const NON_CHARS: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"[^a-z0-9]+").unwrap());
+const ENV_FIREHOSE_STREAM_NAME: &str = "FIREHOSE_STREAM_NAME";
+const ENV_FIREHOSE_SYSTEM_NAME: &str = "FIREHOSE_SYSTEM_NAME";
+const EVENT_BUFFER_SIZE: usize = 8192;
+const EVENT_FLUSH_INVERVAL_SECONDS: u64 = 15;
+const AUTOMATIC_FLUSH_SIZE: usize = 64;
+const MAX_EVENTS_PER_UPLOAD: usize = 256;
 
 #[derive(Serialize)]
 struct EventWrapper<'a, T: Event + Serialize> {
@@ -20,52 +31,91 @@ struct EventWrapper<'a, T: Event + Serialize> {
 }
 
 pub struct FirehoseEventRecorder {
-    client: Client,
-    stream: String,
     system: String,
+    tx: mpsc::Sender<String>,
 }
 
 impl FirehoseEventRecorder {}
 
-const NON_CHARS: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"[^a-z0-9]+").unwrap());
 impl FirehoseEventRecorder {
     #[tracing::instrument(err(Display))]
     pub async fn from_env() -> anyhow::Result<Self> {
         let config = aws_config::load_from_env().await;
         let client = Client::new(&config);
 
-        let stream = env::var("FIREHOSE_STREAM_NAME").unwrap_or_else(|_| {
+        let stream = env::var(ENV_FIREHOSE_STREAM_NAME).unwrap_or_else(|_| {
             NON_CHARS
                 .replace_all(&APP_NAME.to_lowercase(), "_")
                 .into_owned()
         });
-        let system = env::var("FIREHOSE_SYSTEM_NAME").unwrap_or_else(|_| {
+        let system = env::var(ENV_FIREHOSE_SYSTEM_NAME).unwrap_or_else(|_| {
             NON_CHARS
                 .replace_all(&CLUSTER_ID.to_lowercase(), "_")
                 .into_owned()
         });
 
-        Ok(Self {
-            client,
-            stream,
+        tracing::info!(
+            "Sending events to Firehose as system: {} into stream: {}",
             system,
-        })
+            stream
+        );
+
+        let (tx, rx) = mpsc::channel::<String>(EVENT_BUFFER_SIZE);
+        tokio::spawn(async move {
+            run_background_loop(&client, &stream, rx).await;
+        });
+
+        Ok(Self { system, tx })
     }
 
     async fn record_event<'a, E: Event>(&self, event: EventWrapper<'a, E>) -> anyhow::Result<()> {
         let json = serde_json::to_string(&event)?;
-
-        let record = Record::builder().data(json.into_bytes().into()).build()?;
-
-        let _ = self
-            .client
-            .put_record()
-            .delivery_stream_name(&self.stream)
-            .record(record)
-            .send()
-            .await?;
+        self.tx.send(json).await?;
 
         Ok(())
+    }
+}
+
+async fn run_background_loop(client: &Client, stream: &str, mut rx: mpsc::Receiver<String>) {
+    let mut buffer = Vec::new();
+    let mut interval = tokio::time::interval(Duration::from_secs(EVENT_FLUSH_INVERVAL_SECONDS));
+
+    while system::is_running() {
+        tokio::select! {
+            Some(event) = rx.recv() => {
+                buffer.push(event);
+                if buffer.len() >= AUTOMATIC_FLUSH_SIZE {
+                    flush_batch(&client, &stream, &mut buffer).await;
+                }
+            },
+            _ = interval.tick() => {
+                    flush_batch(&client,&stream, &mut buffer).await;
+            }
+        }
+    }
+
+    // Channel closed, flush remaining events...
+    flush_batch(&client, &stream, &mut buffer).await;
+}
+
+#[tracing::instrument(level = "debug", skip(client, buffer))]
+async fn flush_batch(client: &Client, stream: &str, buffer: &mut Vec<String>) {
+    let records = buffer
+        .drain(..)
+        .map(|json| Record::builder().data(json.into_bytes().into()).build())
+        .flatten()
+        .collect::<Vec<_>>();
+
+    for chunk in records.chunks(MAX_EVENTS_PER_UPLOAD) {
+        if let Err(err) = client
+            .put_record_batch()
+            .delivery_stream_name(stream)
+            .set_records(Some(chunk.into()))
+            .send()
+            .await
+        {
+            tracing::error!(?err, "Failed to send batch of events to Firehose");
+        }
     }
 }
 
