cb56a86338dead0ae5cc083f417b0b04562a1338
Andreas Haufler <andreas.haufler@durablox.com>
2025-12-16 21:57:55 +0100

    Permits multiple issuers using JWKS
    We now permit a JWKS_URL to be relative and then check the issuer and append the URI to determine the set of accepted keys.

--- wasabi-core/src/web/auth/authenticator.rs
+++ wasabi-core/src/web/auth/authenticator.rs
@@ -1,7 +1,7 @@
 use crate::status_bail;
 use crate::web::auth::jwks::{JwksCache, UrlJwksFetcher};
 use crate::web::auth::user::ClaimsSet;
-use crate::web::auth::{CLAIM_LOCALE, DEFAULT_LOCALE};
+use crate::web::auth::{CLAIM_ISS, CLAIM_LOCALE, DEFAULT_LOCALE};
 use crate::web::error::ResultExt;
 use anyhow::{Context, bail};
 use async_trait::async_trait;
@@ -9,7 +9,7 @@ use base64::Engine;
 use base64::engine::general_purpose::URL_SAFE_NO_PAD;
 use jsonwebtoken::{Algorithm, DecodingKey, Header, Validation, decode, decode_header};
 use serde_json::Value;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::str::FromStr;
 use std::sync::Arc;
@@ -79,9 +79,9 @@ impl Authenticator {
             .with_status(StatusCode::UNAUTHORIZED)?;
 
         if let Some(config) = self.try_fetch_config(&claims).await {
-            Self::parse_validate_and_update(jwt_token, &config).await
+            Self::parse_validate_and_update(jwt_token, &claims, &config).await
         } else {
-            Self::parse_validate_and_update(jwt_token, &self.config).await
+            Self::parse_validate_and_update(jwt_token, &claims, &self.config).await
         }
     }
 
@@ -101,9 +101,10 @@ impl Authenticator {
 
     async fn parse_validate_and_update(
         jwt_token: &str,
+        claims: &ClaimsSet,
         config: &AuthenticatorConfig,
     ) -> anyhow::Result<ClaimsSet> {
-        let mut claims = config.check_signature(jwt_token).await?;
+        let mut claims = config.check_signature(claims, jwt_token).await?;
 
         if let Some(custom_claim_prefix) = &config.custom_claim_prefix {
             claims = Self::translate_claims(claims, custom_claim_prefix);
@@ -153,7 +154,13 @@ pub struct AuthenticatorConfig {
     validation: Validation,
     default_locale: String,
     custom_claim_prefix: Option<String>,
-    jwks_cache: Option<JwksCache>,
+    jwks_cache: JwksCacheStrategy,
+}
+
+enum JwksCacheStrategy {
+    None,
+    Static(Arc<JwksCache>),
+    PerIssuer(HashMap<String, Arc<JwksCache>>),
 }
 
 impl AuthenticatorConfig {
@@ -166,11 +173,14 @@ impl AuthenticatorConfig {
         default_locale: String,
         custom_claim_prefix: Option<String>,
     ) -> Self {
+        let validation = Self::build_validation(algorithms, issuer, audience);
+        let jwks_cache =
+            Self::build_jwks_cache_strategy(&validation, jwks_url.map(|url| url.to_string()));
+
         AuthenticatorConfig {
             hmac_based_key: shared_secret
                 .map(|str| DecodingKey::from_secret(str.as_ref().as_bytes())),
-            jwks_cache: jwks_url
-                .map(|url| JwksCache::new(Box::new(UrlJwksFetcher::new(url.to_string())))),
+            jwks_cache,
             default_locale,
             custom_claim_prefix,
             validation: Self::build_validation(algorithms, issuer, audience),
@@ -213,6 +223,37 @@ impl AuthenticatorConfig {
         .filter(|set| !set.is_empty())
     }
 
+    fn build_jwks_cache_strategy(
+        validation: &Validation,
+        jwks_url: Option<String>,
+    ) -> JwksCacheStrategy {
+        if let Some(jwks_url) = jwks_url {
+            if jwks_url.starts_with('/') {
+                if let Some(issuers) = &validation.iss {
+                    let caches_per_issuer = issuers
+                        .iter()
+                        .map(|iss| {
+                            let url = format!("{}{}", iss.trim_matches('/'), jwks_url);
+
+                            (iss.to_owned(), Self::build_cache_for_url(url))
+                        })
+                        .collect();
+                    JwksCacheStrategy::PerIssuer(caches_per_issuer)
+                } else {
+                    JwksCacheStrategy::None
+                }
+            } else {
+                JwksCacheStrategy::Static(Self::build_cache_for_url(jwks_url))
+            }
+        } else {
+            JwksCacheStrategy::None
+        }
+    }
+
+    fn build_cache_for_url(url: String) -> Arc<JwksCache> {
+        Arc::new(JwksCache::new(Box::new(UrlJwksFetcher::new(url))))
+    }
+
     fn parse_algorithms(algorithms: &str) -> Vec<Algorithm> {
         Self::parse_set(algorithms)
             .map(|set| {
@@ -223,7 +264,11 @@ impl AuthenticatorConfig {
             .unwrap_or_default()
     }
 
-    async fn check_signature(&self, jwt_token: &str) -> anyhow::Result<ClaimsSet> {
+    async fn check_signature(
+        &self,
+        claims: &ClaimsSet,
+        jwt_token: &str,
+    ) -> anyhow::Result<ClaimsSet> {
         let header = decode_header(jwt_token)
             .context("Invalid JWT present")
             .with_status(StatusCode::UNAUTHORIZED)?;
@@ -238,7 +283,7 @@ impl AuthenticatorConfig {
             );
         }
 
-        if let Some(jwks_cache) = &self.jwks_cache
+        if let Some(jwks_cache) = &self.resolve_jwks_cache(claims).await
             && let Some(kid) = &header.kid
         {
             let decoding_key = jwks_cache
@@ -253,6 +298,18 @@ impl AuthenticatorConfig {
                 StatusCode::UNAUTHORIZED,
                 "Invalid JWT present: No matching authentication mechanism found"
             )
+        }
+    }
+
+    async fn resolve_jwks_cache(&self, claims: &ClaimsSet) -> Option<Arc<JwksCache>> {
+        match &self.jwks_cache {
+            JwksCacheStrategy::None => None,
+            JwksCacheStrategy::Static(cache) => Some(cache.clone()),
+            JwksCacheStrategy::PerIssuer(cache_per_issuer) => claims
+                .get(CLAIM_ISS)
+                .and_then(Value::as_str)
+                .and_then(|iss| cache_per_issuer.get(iss))
+                .cloned(),
         }
     }
 
